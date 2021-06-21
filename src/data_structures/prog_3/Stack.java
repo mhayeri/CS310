4374ed7ebf077bb414034a488a44bfdfd3f02408
Mustafa Hayeri <mustafahayeri@yahoo.com>
2021-06-21 13:31:51 -0700

    added stack/queue implementations

---
+++ src/data_structures/prog_3/Stack.java
@@ -0,0 +1,49 @@
+package data_structures.prog_3;
+
+import java.util.Iterator;
+
+public class Stack<E extends Comparable<E>> implements Iterable<E> {
+
+    private LinearListADT<E> list;
+
+    public Stack() {
+        list = new LinearList<E>();
+    }
+
+    public void push(E obj) {
+        list.addFirst(obj);
+    }
+
+    public E pop() {
+        return list.removeFirst();
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public E peek() {
+        return list.peekFirst();
+    }
+
+    public boolean contains(E obj) {
+        return list.contains(obj);
+    }
+
+    public void makeEmpty() {
+        list.clear();
+    }
+
+    public boolean remove(E obj) {
+        return list.remove(obj) != null;
+    }
+
+    public Iterator<E> iterator() {
+        return list.iterator();
+    }
+
+}
